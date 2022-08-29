ae94355776f3076ac1c3da70af35523104bf0bba
unknown <tarunrakhunde@gmail.com>
2022-08-29 18:02:11 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  arr:any[]=[]
+  addTodo(name:string){
+
+this.arr.push({id:this.arr.length,title:name})
+console.log(this.arr)
+  }
+
+  remove(id:number){
+// console.log(id)
+this.arr=this.arr.filter((e)=>e.id!==id)
+  }
 }
